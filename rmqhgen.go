83b77e4e1280dc67101a501ff11a71b1800e0f13
Guillaume Charmetant <cguille.dev@gmail.com>
2020-01-15 13:59:01 +0100

    Implement rmqhgen

--- rmqhgen.go
+++ rmqhgen.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	var password, algo string
-
-	flag.StringVar(&password, "password", "", "The password to hash")
-	flag.StringVar(&algo, "algorithm", "sha256", "The hash algorithm to use (either sha256 or sha512)")
-
 	flag.Parse()
+	var args = flag.Args()
 
-	fmt.Println("Password to hash:", password)
-	fmt.Println("Hash algorithm:", algo)
+	if len(args) != 1 {
+		fmt.Fprintf(os.Stderr, "Expected exactly one argument (args: %v)\n", args)
+		os.Exit(1)
+	}
+
+	fmt.Println(generate(args[0]))
+}
+
+func generate(password string) string {
+	var salt = generateSalt()
+	var saltedPass = append(salt, password...)
+	var hash = sha256.Sum256(saltedPass)
+	var saltedHash = append(salt, hash[:]...)
+
+	return base64.StdEncoding.EncodeToString(saltedHash)
+}
+
+func generateSalt() []byte {
+	var salt = make([]byte, 4)
+
+	rand.Read(salt)
+
+	return salt
 }

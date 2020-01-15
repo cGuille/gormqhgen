7c2988e1d6579d0a9f31a31605966ba293726b4a
Guillaume Charmetant <cguille.dev@gmail.com>
2020-01-15 17:52:27 +0100

    Implement validate subcommand

--- rmqhgen.go
+++ rmqhgen.go
@@ -25,7 +25,16 @@ func main() {
 		}
 		fmt.Println(generate(subcommandArgs[0]))
 	case "validate":
-		fmt.Println("TODO")
+		if len(subcommandArgs) != 2 {
+			fmt.Fprintf(os.Stderr, "Expected exactly two arguments (args: %v)\n", subcommandArgs)
+			os.Exit(1)
+		}
+		if validate(subcommandArgs[0], subcommandArgs[1]) {
+			fmt.Println("OK")
+		} else {
+			fmt.Println("Invalid password")
+			os.Exit(1)
+		}
 	default:
 		fmt.Println("Expected either 'generate' or 'validate' subcommand")
 		os.Exit(1)
@@ -33,7 +42,22 @@ func main() {
 }
 
 func generate(password string) string {
-	var salt = generateSalt()
+	return generateWithSalt(password, generateSalt())
+}
+
+func validate(password string, hash string) bool {
+	var decoded, err = base64.StdEncoding.DecodeString(hash)
+
+	if err != nil {
+		return false
+	}
+
+	var expectedHash = generateWithSalt(password, decoded[:4])
+
+	return hash == expectedHash
+}
+
+func generateWithSalt(password string, salt []byte) string {
 	var saltedPass = append(salt, password...)
 	var hash = sha256.Sum256(saltedPass)
 	var saltedHash = append(salt, hash[:]...)
